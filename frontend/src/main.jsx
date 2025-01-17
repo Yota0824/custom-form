ac5ba9d18dbba699376ac09671382e12be09579c
Ray.inc <ray.inc@RayincnoMacBook-Pro.local>
2025-01-17 20:41:11 +0900

    ルーティング

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter,Routes,Route } from "react-router";
+import TopPage from './pages/TopPage.jsx';
+import CompletePage from './pages/CompletePage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<TopPage />} />
+        <Route path='/complete' element={<CompletePage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
