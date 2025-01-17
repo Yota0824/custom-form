cef52f55b65ee425639d74cd96856e5a0dddfea7
Ray.inc <ray.inc@RayincnoMacBook-Pro.local>
2025-01-17 21:09:00 +0900

    講義終了

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,12 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    "corsheaders",
     'api',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # 認証
 AUTH_USER_MODEL = "api.Account"
+
+#CORS
+CORS_ALLOW_ALL_ORIGINS = True
--- frontend/src/pages/CompletePage.jsx
+++ frontend/src/pages/CompletePage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const CompletePage = () => {
+    const handleClick = async() =>{
+        console.log("test");
+        const response = await fetch("http://127.0.0.1:8000/test/");
+        const data = await response.json();
+        console.log(data);
+        };
   return (
-    <div>CompletePage</div>
+    <div>
+<button className='bg-blue-500 text-white p-2' onClick={handleClick}>
+    TEST
+
+</button>
+    </div>
   )
 }
 
