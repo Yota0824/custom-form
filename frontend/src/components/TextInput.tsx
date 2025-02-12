c1d8c3efefee73760e5875b066ef69864ff9573b
Ray.inc <ray.inc@RayincnoMacBook-Pro.local>
2025-02-12 23:03:15 +0900

    Add Date

--- frontend/src/components/TextInput.tsx
+++ frontend/src/components/TextInput.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 
-export const TextInput = ({ value, onChange }) => {
+export const TextInput = ({ label,value,onChange,placeholder }) => {
   return (
-    <div>
       <div className="mb-6">
+        <label
+          className="block text-sm font-medium text-gray-700 mb-2"
+        >
+          {label}
+        </label>
         <input
           type="text"
           className="p-3 border rounded-md w-full"
-          placeholder="1300"
+          placeholder={placeholder}
           value={value}
           onChange={onChange}
           required
         />
       </div>
-    </div>
   );
 };
--- frontend/src/components/forms/WebFlyerForm.jsx
+++ frontend/src/components/forms/WebFlyerForm.jsx
@@ -1,31 +1,43 @@
 import { useState } from "react";
-import { SchoolInput } from "../components/ShoolInput";
-import { Button } from "../components/Button";
-import { TextLavel } from "../components/TextLavel";
+import { SchoolInput } from "../ShoolInput";
+import { Button } from "../Button";
+import { TextInput } from "../TextInput";
+import { useNavigate } from "react-router";
+import { DateInput } from "../DateInput";
 
 export const WebFlyerForm = () => {
   const [school_id, set_school_id] = useState("");
   const [apply_name, set_apply_name] = useState("");
+  const navigate = useNavigate();
+  const onSubmit = async () => {
+    navigate("/")
+  };
+  const onClick = () => {
+    navigate("/")
+  }
 
-  const submit = async () => {};
   return (
     <div className="flex items-center justify-center min-h-screen pt-[120px] bg-gray-100">
       <div className="bg-white shadow-lg rounded-lg p-6 max-w-lg w-full">
         <div className="text-xl font-bold">WEB/ビラ集計フォーム</div>
         <p className="text-gray-600 mb-6">aaaaaaaaa</p>
-        <form>
+        <form onSubmit={onSubmit}>
           <SchoolInput
             value={school_id}
             onChange={(e) => set_school_id(e.target.value)}
           />
-          <TextLavel
-            // lavel ={送信者名}
+          <TextInput
+            label="回答者名"
             value={apply_name}
-            placeholder={"武田太郎"}
+            placeholder="武田太郎"
             onChange={(e) => set_apply_name(e.target.value)}
           />
+          <DateInput
+            label="対象年月"
+            />
 
-          <Button onClick={submit}>フォームを回答</Button>
+          <Button type="type">フォームを回答</Button>
+          <Button variant="secandary" type="button" onClick={onClick}>キャンセル</Button>
         </form>
       </div>
     </div>
